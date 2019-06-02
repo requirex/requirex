b9f67608cc9cd1c23cbb706fdb14c42619383325
jjrv <juha@busfaster.com>
2019-06-02 08:33:12 +0300

    :bug: Support Python 3 in example web server.

--- example/browser/serve.bat
+++ example/browser/serve.bat
@@ -3,8 +3,15 @@ echo ; function goto { true; } # > NUL
 
 goto win
 
-command -v busybox > /dev/null && { busybox httpd -fp 8080 && exit 0; }
-command -v python > /dev/null && { python -m SimpleHTTPServer 8080; exit 0; }
+command -v busybox > /dev/null && {
+	busybox httpd -fp 8080 &&
+	exit 0
+}
+
+command -v python > /dev/null && {
+	python -m $(python -c 'import sys; print("SimpleHTTPServer" if sys.version_info.major < 3 else "http.server")') 8080
+	exit 0
+}
 
 exit 1
 
