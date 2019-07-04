62051e58ac7cb2d9dbe713e647ab08bb0fb93813
jjrv <juha@busfaster.com>
2019-07-05 02:14:07 +0300

    :construction: Transpile CSS using PostCSS.

--- src/plugin/CSS.ts
+++ src/plugin/CSS.ts
@@ -1,12 +1,82 @@
+import { URL } from '../URL';
 import { Record } from '../Record';
-import { LoaderPlugin } from '../Loader';
+import { Loader, LoaderPlugin } from '../Loader';
 import { getTags } from '../platform';
+import * as Lib from 'requirex-postcss-bundle';
 
 /** CSS loader plugin. */
 
 export class CSS implements LoaderPlugin {
 
+	constructor(private loader: Loader) { }
+
+	translate(record: Record) {
+		const loader = this.loader;
+		const config = loader.getConfig();
+		const bundleName = 'requirex-postcss-bundle';
+
+		record.moduleInternal = {
+			exports: {},
+			id: record.resolvedKey
+		};
+
+		if(!this.builder && config.postCSS) {
+			this.builder = loader.import(
+				bundleName,
+				loader.baseURL || loader.firstParent
+			).then(
+				() => loader.import(bundleName + '/src/index.ts')
+			).then((lib: typeof Lib) => new lib.PostBuilder({
+				importResolve: (key: string, dir: string) => {
+					// TODO: Resolve http URLs?
+					dir = dir.replace(/\/?$/, '/');
+					console.log('CSS RESOLVE', key, dir, URL.resolve(dir, key));
+
+					// NOTE: Promise wrapper is just for testing that returning promises works.
+					return(Promise.resolve(URL.resolve(dir, key)));
+				},
+				importLoad: (key: string) => {
+					const resolvedKey = URL.resolve(
+						loader.baseURL || loader.firstParent!,
+						key
+					);
+
+					console.log('CSS LOAD', key, resolvedKey);
+					// TODO: Use System.import?
+					// TODO: Load http URLs?
+
+					return loader.fetch(resolvedKey).then(
+						(res) => res.ok ? res.text() : Promise.reject(res)
+					);
+				},
+				urlResolve: (key: string, isLocal: boolean) => URL.relative(
+					loader.baseURL || loader.firstParent!,
+					isLocal ? URL.fromLocal(key) : key
+				),
+				minify: config.minifyCSS
+			}));
+		}
+
+		if(!this.builder) return;
+
+		return this.builder.then((builder: Lib.PostBuilder) => {
+			console.log(builder);
+			return builder.build(
+				URL.toLocal(record.resolvedKey),
+				URL.toLocal(loader.baseURL || loader.firstParent!)
+			);
+		}).then((code: string) => {
+			record.sourceCode = code;
+		}).catch((err: any) => {
+			console.error(err);
+		});
+	}
+
 	instantiate(record: Record) {
+if(record.sourceCode) {
+	console.log(record.sourceCode);
+}
+
 		if(!getTags) return;
 		const head = getTags('head')[0];
 
@@ -21,5 +91,7 @@ export class CSS implements LoaderPlugin {
 
 		head.appendChild(element);
 	}
+
+	builder: Promise<Lib.PostBuilder>;
 
 }
